369a73f617d483feda0b55bd1f513452d46241e3
pragati <pragatikunwer@gmail.com>
2016-02-26 23:27:23 -0800

    Added Twitter Bootstrap Support

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -2,28 +2,32 @@
 
 /* App Module */
 
-var helloWorldApp = angular.module('helloWorldApp', [ 'ngRoute',
-		'helloWorldControllers'
-
-]);
-
-helloWorldApp.config([ '$routeProvider', '$locationProvider',
+var blogApp = angular.module('blogApp', [ 'ngRoute', 'blogControllers' ]);
+blogApp.config([ '$routeProvider', '$locationProvider',
 		function($routeProvider, $locationProvider) {
 			$routeProvider.when('/', {
 				templateUrl : 'partials/main.html',
-				controller : 'MainCtrl'
-			}).when('/show', {
-				templateUrl : 'partials/show.html',
-				controller : 'ShowCtrl'
-			}).when('/customer', {
-				templateUrl : 'partials/customer.html',
-				controller : 'CustomerCtrl'
-			}).when('/addCustomer', {
-				templateUrl : 'partials/newCustomer.html',
-				controller : 'AddCustomerCtrl'
-			}).when('/addedCustomer/:customer/:city', {
-				templateUrl : 'partials/addedCustomer.html',
-				controller : 'AddedCustomerCtrl'
+				controller : 'BlogCtrl'
+			}).when('/blogPost/:id', {
+				templateUrl : 'partials/blogPost.html',
+				controller : 'BlogViewCtrl'
 			});
 			$locationProvider.html5Mode(false).hashPrefix('!');
 		} ]);
+
+/*
+ * var helloWorldApp = angular.module('helloWorldApp', [ 'ngRoute',
+ * 'helloWorldControllers' ]);
+ * 
+ * helloWorldApp.config([ '$routeProvider', '$locationProvider',
+ * 
+ * function($routeProvider, $locationProvider) { $routeProvider.when('/', {
+ * templateUrl : 'partials/main.html', controller : 'MainCtrl' }).when('/show', {
+ * templateUrl : 'partials/show.html', controller : 'ShowCtrl'
+ * }).when('/customer', { templateUrl : 'partials/customer.html', controller :
+ * 'CustomerCtrl' }).when('/addCustomer', { templateUrl :
+ * 'partials/newCustomer.html', controller : 'AddCustomerCtrl'
+ * }).when('/addedCustomer/:customer/:city', { templateUrl :
+ * 'partials/addedCustomer.html', controller : 'AddedCustomerCtrl' });
+ * $locationProvider.html5Mode(false).hashPrefix('!'); } ]);
+ */
